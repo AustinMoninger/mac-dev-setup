ae1c0ec3cd713b36fc95d914a964f547bfa08d7b
Austin Moninger <acm9@rice.edu>
2018-07-01 15:41:47 -0600

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,18 @@
+# Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
+brew doctor
+
+# Install consolas font
+brew install cabextract
+cd ~/Downloads
+mkdir consolas
+cd consolas
+curl -O http://download.microsoft.com/download/f/5/a/f5a3df76-d856-4a61-a6bd-722f52a5be26/PowerPointViewer.exe
+cabextract PowerPointViewer.exe
+cabextract ppviewer.cab
+open CONSOLA*.TTF
+
+# Install git with some colors
+brew install git
+git --version
